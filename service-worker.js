45946937959fc42471fd60dc014564c224526bbb
EnBest2 <enbest2@gmail.com>
2025-06-03 13:56:38 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
 const CACHE_NAME = "payslips-cache-v1";
 const urlsToCache = [
   "index.html",
-  "viewer.html",
   "manifest.json",
   "service-worker.js",
   "icon.png"
@@ -9,27 +8,23 @@ const urlsToCache = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
+    caches.keys().then(cacheNames =>
+      Promise.all(
         cacheNames.filter(name => name !== CACHE_NAME)
           .map(name => caches.delete(name))
-      );
-    })
+      )
+    )
   );
 });
